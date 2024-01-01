3ebb30d740f0e4c9412b25886a1ae52c514474af
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2024-01-01 11:18:13 +0300

    learn ang

--- Udemy-Angular - The Complete Guide (2022 Edition)/script.js
+++ Udemy-Angular - The Complete Guide (2022 Edition)/script.js
@@ -7,5 +7,78 @@
 
 //при подключений какой то фичи нам необходимо в angular подлючать конкретные модули (субпакеты)
 
+//----------------------------
+//__Базовая работа__//
+//при загрузку получает скрипты которые подгружет потом на странице
+
+//index.html
+//<app-root></app-root>
+
+//cтартовая точка приложения подлючаем все модули
+//main.ts
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+//настройки модулей / подключения и т.д
+//app.module.ts
+
+//набор функциональных возможностей приложения
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+//app.component.ts
+//@класический декоратор внутри которого передаем объект с параметрами чтобы на выходе изменить класс
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app';
+}
+
+/* 
+app.component.html
+//ngModel работает т.к подключили FormsModule 
+<input type="text" [(ngModel)] ='title'>
+<h1>{{title}}</h1> - значения инпутав title по умолчанию app
+
+app.component.scss
+...
+*/
+
+//___Создание компонент__//
+
+//можно создавать компоненту через терминал спец команда ng generate component .../ng g c ...
+
+//можно сразу писать разметку в template- плохая практика
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <h1>hello<h1/>
+  `,
+  styles: [
+    `
+    .title{}
+  `
+  ],
+  imports: [Test1Module],
+})
+export class AppComponent {
+  title = 'app';
+}
+
 
 
