6c76f8d034e962324ec24a9b398d9a8220195a58
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2024-01-14 18:09:08 +0300

    practice

--- learn/script.js
+++ learn/script.js
@@ -507,5 +507,43 @@ export class AppComponent {
 
 //<h1 [ngStyle]="{color:'blue'}">hell!!!</h1>
 
-//-------------
-//__
+//-----------------------------------
+//__Задача на проработку директив__//
+
+interface Fruit {
+  id: string
+  name: string
+  price: number
+}
+
+@Component({
+  selector: 'main-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  fruits: Fruit[] = [
+    { id: '1', name: 'apple', price: 10 },
+    { id: '2', name: 'orange', price: 20 },
+    { id: '3', name: 'watermelon', price: 30 },
+    { id: '4', name: 'banana', price: 5 },
+    { id: '5', name: 'pears', price: 12 },
+    { id: '6', name: 'raspberries', price: 18 },
+    { id: '7', name: 'avocados', price: 14 },
+    { id: '8', name: 'mangoes', price: 3 },
+    { id: '9', name: 'kiwifruit', price: 7 },
+  ];
+}
+/* 
+<div *ngFor="let fr of fruits; index as i; even as ev; odd as od" [ngClass]="{
+  'default': fr.price > 15,
+  'success': ev ,
+  'error': od 
+}">
+  <ng-container *ngIf="fr.price > 7">
+    {{ fr.name }} {{ fr.price }}
+  </ng-container>
+</div>
+*/
+
+
--- my-first-project/src/app/app.component.ts
+++ my-first-project/src/app/app.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 
+interface Fruit {
+  id: string
+  name: string
+  price: number
+}
+
 @Component({
   selector: 'main-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  isError:boolean = false
-
-  constructor (){
-    setTimeout(() => {
-      this.isError = true
-    }, 3000);
-  }
+  fruits: Fruit[] = [
+    { id: '1', name: 'apple', price: 10 },
+    { id: '2', name: 'orange', price: 20 },
+    { id: '3', name: 'watermelon', price: 30 },
+    { id: '4', name: 'banana', price: 5 },
+    { id: '5', name: 'pears', price: 12 },
+    { id: '6', name: 'raspberries', price: 18 },
+    { id: '7', name: 'avocados', price: 14 },
+    { id: '8', name: 'mangoes', price: 3 },
+    { id: '9', name: 'kiwifruit', price: 7 },
+  ];
 }
