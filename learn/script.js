92fe377b0bd6c49b15bc28b033b679bcd56d53df
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2024-01-14 16:49:06 +0300

    NgFor hard structure

--- learn/script.js
+++ learn/script.js
@@ -351,3 +351,74 @@ export class AppComponent {
 </div>
 */
 
+//------------------------------------
+//__NgFor сложная структура данных__//
+
+interface Item {
+  id: number;
+  name: string;
+}
+
+interface NestedArray {
+  id: number;
+  name: string;
+  items: Item[];
+}
+
+@Component({
+  selector: 'main-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  nestedArray: NestedArray[] = [
+    {
+      id: 1,
+      name: 'Parent 1',
+      items: [
+        {
+          id: 11,
+          name: 'Child 1.1',
+        },
+        {
+          id: 12,
+          name: 'Child 1.2',
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Parent 2',
+      items: [
+        {
+          id: 21,
+          name: 'Child 2.1',
+        },
+        {
+          id: 22,
+          name: 'Child 2.2',
+        },
+      ],
+    },
+  ];
+
+}
+/* 
+<div *ngFor="let items of nestedArray">
+  {{items.name}}
+  <div *ngFor="let item of items.items">
+    {{item.name}}
+  </div>
+</div>
+*/
+
+/* 
+Parent 1
+Child 1.1
+Child 1.2
+Parent 2
+Child 2.1
+Child 2.2
+*/
+
+//__
--- my-first-project/src/app/app.component.ts
+++ my-first-project/src/app/app.component.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  id: number;
+  name: string;
+}
+
+interface NestedArray {
+  id: number;
+  name: string;
+  items: Item[];
+}
+
 @Component({
   selector: 'main-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  arr:string[]= []
+  nestedArray: NestedArray[] = [
+    {
+      id: 1,
+      name: 'Parent 1',
+      items: [
+        {
+          id: 11,
+          name: 'Child 1.1',
+        },
+        {
+          id: 12,
+          name: 'Child 1.2',
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Parent 2',
+      items: [
+        {
+          id: 21,
+          name: 'Child 2.1',
+        },
+        {
+          id: 22,
+          name: 'Child 2.2',
+        },
+      ],
+    },
+  ];
 
-  sendvalue (value:string){
-    this.arr.push(value);
-  }
 }
